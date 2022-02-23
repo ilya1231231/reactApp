fc267f12a1afd57ac9d45998b83e3b36dbe07901
ilya <malygin.ilja@yandex.ru>
2022-02-23 16:37:53 +0300

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+  <div>
+    <h1>hello world</h1>
+    <Button variant="primary">Button</Button>
+  </div>
+  );
+}
+
+export default App;
