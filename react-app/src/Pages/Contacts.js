20c36d2ae8d8d9e0bedce92979cc796559419b94
ilya <malygin.ilja@yandex.ru>
2022-02-26 12:35:43 +0300

    добабвил форму обратной связи

--- react-app/src/Pages/Contacts.js
+++ react-app/src/Pages/Contacts.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
 
 export default class Contacts extends Component {
   render() {
     return (
-      <div>
-        Contacts
-      </div>
+      <Container style={{width:'500px'}} className="mt-3">
+        <h1 className="text-center">Свяжитесь с нами</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Введите email"/>
+            <Form.Text>
+              Мы не отправим ваш Email никому
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword" className='mt-3'>
+            <Form.Label>Напишите что думаете о нас</Form.Label>
+            <Form.Control as="textarea" rows="3" placeholder="Введите email"/>
+          </Form.Group>
+
+          <Form.Group controlId="check">
+            <Form.Check type="checkbox" label="Check me out"/>
+          </Form.Group>
+
+          <Button variant="primary">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     );
   }
 }
